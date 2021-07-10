48221a9f797cb06a145c4fcbff4508ddf0d941f4
Colston <bodoycolstond@gmail.com>
2021-07-10 20:52:46 +0800

    Update src folder and README.md
    Added List tutorial references

--- README.md
+++ README.md
@@ -131,3 +131,56 @@ const Book = (props) => {
 	);
 };
 ```
+
+## List
+
+**Note: Each child in a list should have a unique "key" prop.**
+
+**Example:**
+
+```javascript
+const books = [
+	{
+		id: 1,
+		img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_US218_..jpg",
+		title: "I Love You to the Moon and Back",
+		author: "Amelia Hepworth",
+	},
+
+	{
+		id: 2,
+		img: "https://m.media-amazon.com/images/I/818qxyeFS-L._AC_US218_..jpg",
+		title: "What Are Unicorns Made Of?",
+		author: "Amelia Hepworth",
+	},
+
+	{
+		id: 3,
+		img: "https://m.media-amazon.com/images/I/71q5k7wq-CL._AC_US218_..jpg",
+		title: "ROAR!: WARNING! This book is very NOISY!",
+		author: "Amelia Hepworth",
+	},
+];
+
+const BookList = () => {
+	return (
+		<section className="booklist">
+			{books.map((book) => {
+				return <Book key={book.id} {...book}></Book>;
+			})}
+		</section>
+	);
+};
+
+const Book = (props) => {
+	const { img, title, author } = props;
+
+	return (
+		<article className="book">
+			<img src={img} alt="" />
+			<h1>{title}</h1>
+			<h4>{author}</h4>
+		</article>
+	);
+};
+```
--- src/index.js
+++ src/index.js
@@ -2,48 +2,35 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const firstBook = {
-	img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_US218_..jpg",
-	title: "I Love You to the Moon and Back",
-	author: "Amelia Hepworth",
-};
+const books = [
+	{
+		id: 1,
+		img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_US218_..jpg",
+		title: "I Love You to the Moon and Back",
+		author: "Amelia Hepworth",
+	},
 
-const secondBook = {
-	img: "https://m.media-amazon.com/images/I/818qxyeFS-L._AC_US218_..jpg",
-	title: "What Are Unicorns Made Of?",
-	author: "Amelia Hepworth",
-};
+	{
+		id: 2,
+		img: "https://m.media-amazon.com/images/I/818qxyeFS-L._AC_US218_..jpg",
+		title: "What Are Unicorns Made Of?",
+		author: "Amelia Hepworth",
+	},
 
-const thirdBook = {
-	img: "https://m.media-amazon.com/images/I/71q5k7wq-CL._AC_US218_..jpg",
-	title: "ROAR!: WARNING! This book is very NOISY!",
-	author: "Amelia Hepworth",
-};
+	{
+		id: 3,
+		img: "https://m.media-amazon.com/images/I/71q5k7wq-CL._AC_US218_..jpg",
+		title: "ROAR!: WARNING! This book is very NOISY!",
+		author: "Amelia Hepworth",
+	},
+];
 
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
-			>
-				<p>
-					Show a child just how strong your love is every minute of the day!
-					Features a "To" and "From" personalization page, making this sweet
-					offering an ideal gift for baby showers, birthdays, and new parents.
-				</p>
-			</Book>
-			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author}
-			/>
-			<Book
-				img={thirdBook.img}
-				title={thirdBook.title}
-				author={thirdBook.author}
-			/>
+			{books.map((book) => {
+				return <Book key={book.id} {...book}></Book>;
+			})}
 		</section>
 	);
 };
@@ -56,7 +43,6 @@ const Book = (props) => {
 			<img src={img} alt="" />
 			<h1>{title}</h1>
 			<h4>{author}</h4>
-			{props.children}
 		</article>
 	);
 };
